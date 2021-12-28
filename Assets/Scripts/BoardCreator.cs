06e363b6bab9020ae31f355841b9103c9facfcd6
erselyikar <yikarstudio@gmail.com>
2021-12-28 17:05:24 +0300

    refactor: TilePooler and BoardCreator scripts

--- Assets/Scripts/BoardCreator.cs
+++ Assets/Scripts/BoardCreator.cs
@@ -4,20 +4,38 @@ using UnityEngine;
 
 public class BoardCreator : MonoBehaviour
 {
-    [SerializeField] private Sprite _boardTile;
+    private GameObject _tile;
     private int xTileNumber = 8;
     private int yTileNumber = 8;
+    private int yMaxNumber = 2;
+    private float farLeft = -3.5f;
 
     void Start()
     {
-        
+        CreateBoard();
     }
 
     private void CreateBoard()
     {
         for(int i = 0; i < xTileNumber; i++)
         {
+            for(int j = 0; j < yTileNumber; j++)
+            {
+                _tile = TilePooler.TileInstance.GetTileFromPool();
 
+                if (_tile != null)
+                {
+                    _tile.SetActive(true);
+                }
+                
+                for(int y = 3; y > yMaxNumber; y--)
+                {
+                    _tile.transform.position = new Vector2(farLeft, y);
+                }
+                yMaxNumber--;
+            }
+            farLeft++;
+            yMaxNumber = 2;
         }
     }
 }
--- Assets/Scripts/TilePooler.cs
+++ Assets/Scripts/TilePooler.cs
@@ -5,23 +5,41 @@ using UnityEngine;
 public class TilePooler : MonoBehaviour
 {
     public static TilePooler TileInstance;
-    public List<Sprite> tileSprites;
-    public Sprite tileToPool;
+    public List<GameObject> tileObjects;
+    [SerializeField] private GameObject tileToPool;
+    [SerializeField] private GameObject board;
+    private int poolAmount = 64;
 
     private void Awake()
     {
         TileInstance = this;
     }
 
-    // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
-        
+        CreatePool();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreatePool()
     {
-        
+        for(int i = 0; i < poolAmount; i++)
+        {
+            GameObject tile = Instantiate(tileToPool);
+            tile.SetActive(false);
+            tileObjects.Add(tile);
+            tile.transform.parent = board.transform;
+        }
+    }
+
+    public GameObject GetTileFromPool()
+    {
+        for(int i = 0; i <tileObjects.Count; i++)
+        {
+            if (!tileObjects[i].activeInHierarchy)
+            {
+                return tileObjects[i];
+            }
+        }
+        return null;
     }
 }
